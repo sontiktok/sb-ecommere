1464bb79b0b4d598fc8e40db16b133e64eac8d5f
sontiktok <sonvltiktok@gmail.com>
2024-09-01 16:38:09 +0700

    Custom responseDTO: APIResponse

--- src/main/java/com/ecommere/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommere/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommere.project.exceptions;
 
+import com.ecommere.project.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -27,14 +28,19 @@ public class MyGlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
-        String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFoundException e){
+        APIResponse apiResponse = new APIResponse();
+        apiResponse.setMessage(e.getMessage());
+        apiResponse.setStatus(false);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException e){
-        String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<APIResponse> myAPIException(APIException e){
+        APIResponse apiResponse = new APIResponse();
+        apiResponse.setMessage(e.getMessage());
+        apiResponse.setStatus(false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 }
