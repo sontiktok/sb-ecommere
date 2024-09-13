3f864541ebe6003f2af0bc14fa9c824c98f2a035
sontiktok <sonvltiktok@gmail.com>
2024-09-13 23:26:16 +0700

    create model address/user/role and set relationship

--- src/main/java/com/ecommere/project/model/Category.java
+++ src/main/java/com/ecommere/project/model/Category.java
@@ -9,18 +9,22 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Entity(name="categories")
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
     @NotBlank
     @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    //Bi-directional đứng ở category có thể lấy được các sản phẩm tương ức
+    //Khi sửa/xóa/update ở category thì product cũng sẽ ăn theo
     private List<Product> products;
 }
--- src/main/java/com/ecommere/project/model/Product.java
+++ src/main/java/com/ecommere/project/model/Product.java
@@ -1,22 +1,31 @@
 package com.ecommere.project.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Entity(name = "products")
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain atleast 3 characters")
     private String productName;
-    private String description;
     private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain atleast 6 characters")
+    private String description;
     private Integer quantity;
     private double price;
     private double discount;
@@ -24,5 +33,10 @@ public class Product {
 
     @ManyToOne
     @JoinColumn(name = "category_id")
+    //Bi-directional, đứng ở sản phẩm có thể lấy ra được category tương ứng
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
