b93a90f4517e447d97a89d9073152dd3d3c77c50
sontiktok <sonvltiktok@gmail.com>
2024-09-14 16:00:01 +0700

    create user repository and UserDetailsServiceImpl

---
+++ src/main/java/com/ecommere/project/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ecommere.project.security.services;
+
+import com.ecommere.project.model.User;
+import com.ecommere.project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
